90dc0310e4f80d889b16fb7508cdea4d9e3e0f7e
Cody Wilson <Cody.Wilson@wallawalla.edu>
2016-05-30 14:56:15 -0700

    Added new functionality
    Took code from the book here: http://bcs.wiley.com/he-bcs/Books?action=resource&bcsId=2949&itemId=0471467553&resourceId=7105&chapterId=21532
    Added modularity
    Note, add all files to the project for it to build correctly.

---
+++ Tree_node.h
@@ -0,0 +1,38 @@
+#include <sstream>
+#include <sstream>
+
+using namespace std;
+
+//This is the example Binary Tree Node from page 458.
+template<typename Item_Type>
+struct Tree_node
+{
+	Item_Type data;
+	Tree_node<Item_Type>* left;
+	Tree_node<Item_Type>* right;
+
+	//Constructor
+	Tree_node(const Item_Type& the_data,
+		Tree_node<Item_Type>* left_val = NULL,
+		Tree_node<Item_Type>* right_val = NULL) : data(the_data), left(left_val), right(right_val)
+	{ }
+
+	//Deconstructor
+	virtual ~Tree_node() { }
+
+	//to-string
+	virtual string to_string() const {
+		ostringstream os;
+			os << data;
+		return os.str();
+	}
+};
+
+//Overloading an ostream insertion operator
+template<typename Item_Type>
+ostream& operator<<(ostream& out, const Tree_node <Item_Type>& node)
+{
+	return out << node.to_string();
+}
+using namespace std;
+
